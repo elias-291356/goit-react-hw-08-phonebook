bbf650ec42930f40df68fb6cfd6ba78ce69d8a3f
elias-291356 <136002743+elias-291356@users.noreply.github.com>
2023-10-20 12:56:14 +0000

    Deploying to gh-pages from @ elias-291356/goit-react-hw-08-phonebook@d6af8d6a6aec6dc9bab15a33496430ead92f59a6 🚀

---
+++ static/js/533.60085015.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[533],{533:function(e,t,c){c.r(t);c(791);var r=c(184);t.default=function(){return(0,r.jsx)("div",{children:"contacts"})}}}]);
+//# sourceMappingURL=533.60085015.chunk.js.map
